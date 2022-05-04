c49483bb38b1d0460f8f6a94a62258fec49a8bdf
Enoch <enoch4kigwila@gmail.com>
2022-05-04 13:57:45 -0400

    in app.js: added const http = require('http'); to the top. moved db.js to routes directory.

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 'use strict';
+const http = require('http');
 
 const hostName = '127.0.0.1';
 const port = 3000;
 
-const express = require('express'),
-es6Renderer = require('express-es6-template-engine');
 
+const express = require('express');
 const app = express();
+const es6Renderer = require('express-es6-template-engine');
+
+// const app = express();
 
 app.engine('html', es6Renderer);
 app.set('views', './views');
